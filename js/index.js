d8fdc270d7fa65375446d808d6052669dcb6ae2b
Sinner93 <chepukhin.sergey@gmail.com>
2020-07-07 13:17:12 +0500

    rename folder

--- js/index.js
+++
@@ -1,8 +0,0 @@
-const cellsArray = document.getElementsByClassName('calendar__cell');
-
-for (const cell of cellsArray) {
-  console.log(1);
-  cell.addEventListener('click', () => {
-    console.log(`${cell} click!`);
-  });
-}
