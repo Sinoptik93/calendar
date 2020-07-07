2739d528caf89cde43869e0db329fc297083aa24
Sinner93 <chepukhin.sergey@gmail.com>
2020-07-07 16:47:15 +0500

    add dist folder to gh-pages repository

---
+++ dist/bundle.js
@@ -0,0 +1,163 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = "./src/index.js");
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/*! no exports provided */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _modules_calendarGenerator_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./modules/calendarGenerator.js */ "./src/modules/calendarGenerator.js");
+
+var currentDate = new Date(Date.now());
+console.log('Current Date');
+console.group();
+console.log(currentDate);
+console.groupEnd();
+Object(_modules_calendarGenerator_js__WEBPACK_IMPORTED_MODULE_0__["default"])(currentDate);
+
+/***/ }),
+
+/***/ "./src/modules/calendarGenerator.js":
+/*!******************************************!*\
+  !*** ./src/modules/calendarGenerator.js ***!
+  \******************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+var monthsOfTheYear = ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'];
+var daysOfTheWeek = ["Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"];
+{
+  /* <td class="calendar__cell">
+   <div class="calendar__cell_header">6</div>
+   <div class="calendar__cell_task_body"></div>
+  </td> */
+}
+
+var createElement = function createElement(elementTagName, className) {
+  var newElement = document.createElement(elementTagName);
+  newElement.setAttribute('class', className);
+  return newElement;
+};
+
+var createTaskCell = function createTaskCell(day) {
+  var cellBody = createElement('td', 'calendar__cell');
+  var cellHeader = createElement('div', 'calendar__cell_header');
+  cellHeader.textContent = day;
+  var cellTaskBody = createElement('div', 'calendar__cell_task_body');
+  cellBody.appendChild(cellHeader);
+  cellBody.appendChild(cellTaskBody);
+  return cellBody;
+};
+
+var generateCalendar = function generateCalendar(date) {
+  var currentMonth = date.getMonth();
+  console.log(currentMonth);
+  var currentWeekDay = date.getDay();
+  console.log(currentWeekDay);
+  var currentDay = date.getDate();
+  console.log(currentDay);
+  var daysInMonth = date.setMonth(date.getMonth() + 1);
+  daysInMonth.setDate();
+  console.log('generateCalendar');
+  console.group();
+  console.log(daysInMonth);
+  console.groupEnd();
+};
+
+/* harmony default export */ __webpack_exports__["default"] = (generateCalendar);
+
+/***/ })
+
+/******/ });
+//# sourceMappingURL=bundle.js.map
