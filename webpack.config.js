76611640d33b94a221a9886ff6bea71591146a2a
Sinner93 <chepukhin.sergey@gmail.com>
2020-07-07 14:13:12 +0500

    add webpack for js modules

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+/* eslint-disable no-undef */
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  devtool: 'source-map',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  }
+};
